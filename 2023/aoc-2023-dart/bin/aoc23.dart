44931332da6562a0282e02d699e71c5464a4c211
Miffi <miffi@decadent.rest>
2024-11-24 02:49:39 +0800

    Initial AoC 2023 Dart directory

---
+++ 2023/aoc-2023-dart/bin/aoc23.dart
@@ -0,0 +1,5 @@
+import 'package:aoc23/aoc23.dart' as aoc23;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aoc23.calculate()}!');
+}
---
+++ 2023/aoc-2023-dart/test/aoc23.dart
@@ -0,0 +1,8 @@
+import 'package:aoc23/aoc23.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
