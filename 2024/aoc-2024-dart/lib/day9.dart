30df0d61c17b2ca4440211a2348c49fb4e9c406c
Miffi <miffi@decadent.rest>
2024-12-11 02:49:05 +0800

    dart: 2024 day 9

--- 2024/aoc-2024-dart/lib/day9.dart
+++ 2024/aoc-2024-dart/lib/day9.dart
@@ -1,19 +1,26 @@
 import 'dart:convert';
 import 'dart:math';
 
-(List<int>, List<int>) parse(String input) {
+(List<int>, List<int>, List<int>, List<int>) parse(String input) {
+  var base = 0;
   var filled = <int>[];
+  var filledBases = <int>[];
   var free = <int>[];
+  var freeBases = <int>[];
   var cleanedLine = LineSplitter().convert(input)[0];
   for (int i = 0; i < cleanedLine.length; i++) {
     int val = int.parse(cleanedLine[i]);
     if (i % 2 == 0) {
       filled.add(val);
+      filledBases.add(base);
+      base += val;
     } else {
       free.add(val);
+      freeBases.add(base);
+      base += val;
     }
   }
-  return (filled, free);
+  return (filled, filledBases, free, freeBases);
 }
 
 int sumRange(int offset, int width) {
@@ -25,7 +32,7 @@ int sumRange(int offset, int width) {
 }
 
 int answer1(String input) {
-  var (filled, free) = parse(input);
+  var (filled, _, free, _) = parse(input);
   var left = 0;
   var right = filled.length - 1;
   var acc = 0;
@@ -56,5 +63,24 @@ int answer1(String input) {
 }
 
 int answer2(String input) {
-  return 0;
+  var (filled, filledBases, free, freeBases) = parse(input);
+
+  var acc = 0;
+  for (int i = filled.length - 1; i >= 0; i--) {
+    var relocated = false;
+    for (int j = 0; freeBases[j] < filledBases[i]; j++) {
+      if (free[j] >= filled[i]) {
+        acc += i * sumRange(freeBases[j], filled[i]);
+        free[j] -= filled[i];
+        freeBases[j] += filled[i];
+        relocated = true;
+        break;
+      }
+    }
+    if (!relocated) {
+      acc += i * sumRange(filledBases[i], filled[i]);
+    }
+  }
+
+  return acc;
 }
